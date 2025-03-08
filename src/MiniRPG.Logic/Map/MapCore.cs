b614ea4803e5840a43211fef02763e1d11877ff1
Sam Smith <samsmithnz@gmail.com>
2025-03-08 06:26:45 -0500

    Added map code and tests

---
+++ src/MiniRPG.Logic/Map/MapCore.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace MiniRPG.Logic.Map
+{
+    public class MapCore
+    {
+        /// <summary>
+        /// Initialize a x by y map array
+        /// </summary>
+        /// <param name="xMax">x size</param>
+        /// <param name="yMax">y size</param>
+        /// <param name="initialString">The initial string to initialize the map with - usually ""</param>
+        /// <returns>The populated map/array</returns>
+        public static string[,] InitializeMap(int xMax, int yMax, string initialString = "")
+        {
+            string[,] map = new string[xMax, yMax];
+
+            //Initialize the map
+            for (int y = 0; y < yMax; y++)
+            {
+                for (int x = 0; x < xMax; x++)
+                {
+                    map[x, y] = initialString;
+                }
+            }
+
+            return map;
+        }
+
+        public static string GetMapString(string[,] map, bool stripOutBlanks = false)
+        {
+            int xMax = map.GetLength(0);
+            int yMax = map.GetLength(1);
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Environment.NewLine);
+            for (int y = yMax - 1; y >= 0; y--)
+            {
+                StringBuilder sbLine = new StringBuilder();
+                for (int x = 0; x < xMax; x++)
+                {
+                    if (map[x, y] != "")
+                    {
+                        sbLine.Append(map[x, y] + " ");
+                    }
+                    else
+                    {
+                        sbLine.Append(". ");
+                    }
+                }
+                sbLine.Append(Environment.NewLine);
+                //If there is nothing on the map line, don't display it.
+                //This optimizes the ASCII maps to remove white space
+                if (stripOutBlanks)
+                {
+                    int dotCount = sbLine.ToString().Split('.').Count() - 1;
+                    if (dotCount < xMax)
+                    {
+                        sb.Append(sbLine.ToString());
+                    }
+                }
+                else
+                {
+                    sb.Append(sbLine.ToString());
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static string[,] AddTileTypesToMap(string[,] map, Dictionary<Vector3, string> tileTypeList)
+        {
+            //loop through the tileTypeList and add the tiles to the map
+            foreach (KeyValuePair<Vector3, string> item in tileTypeList)
+            {
+                //Check that the square is empty - we don't want to overwrite something that exists and only put a tile on an unused tile
+                if (map[(int)item.Key.X, (int)item.Key.Y] == "")
+                {
+                    map[(int)item.Key.X, (int)item.Key.Y] = item.Value;
+                }
+            }
+            return map;
+        }
+    }
+}
---
+++ src/MiniRPG.Logic/Map/MapTile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Specialized;
+using System.Numerics;
+
+namespace MiniRPG.Logic.Map
+{
+    public class MapTile
+    {
+        public MapTile(int x, int y, string tileType, string tileAir)
+        {
+            this.Location = new Vector3(x, y, 0);
+            TileType = tileType;
+            TileAir = tileAir;
+        }
+
+        public Vector3 Location { get; private set; }
+        public string TileType { get; set; }
+        public string TileAir { get; set; } = null;
+    }
+}
