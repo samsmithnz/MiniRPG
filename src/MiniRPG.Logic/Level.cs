90920267721ed192eead49390c7940389cba5621
Sam Smith <samsmithnz@gmail.com>
2025-03-11 19:47:03 -0400

    Added level 1 and 2 to logic

--- src/MiniRPG.Logic/Level.cs
+++ src/MiniRPG.Logic/Level.cs
@@ -1,17 +1,98 @@
-﻿//using System.Numerics;
+﻿using MiniRPG.Logic.Map;
+using System.Numerics;
 
-//namespace MiniRPG.Logic
-//{
-//    public class Level
-//    {
-//        public string[,] Map { get; set; }
-//        public Character Character { get; set; }
-//        public Vector3 CharacterStartingLocation { get; set; }
-//        public Level(string[,] map, Character character, Vector3 characterStartingLocation)
-//        {
-//            Map = map;
-//            Character = character;
-//            CharacterStartingLocation = characterStartingLocation;
-//        }
-//    }
-//}
+namespace MiniRPG.Logic
+{
+    public class Level
+    {
+        public int LevelNumber { get; set; }
+        public Character Character { get; set; }
+        public Vector3 StartingLocation { get; set; }
+        public Vector3 EndingLocation { get; set; }
+        public string[,] Map { get; set; }
+
+        public Level(int levelNumber, 
+            Character character, 
+            Vector3 startingLocation,
+            Vector3 endingLocation)
+        {
+            LevelNumber = levelNumber;
+            Character = character;
+            StartingLocation = startingLocation;
+            EndingLocation = endingLocation;
+
+            if (levelNumber == 1)
+            {
+                Level1();
+            }
+            else if (levelNumber == 2)
+            {
+                Level2();
+            }
+        }
+
+        private void Level1()
+        {
+            int xMax = 9;
+            int zMax = 9;
+            string[,] map = MapCore.InitializeMap(xMax, zMax);
+            Vector3 startingLocation = new Vector3(4, 0, 0);
+            Vector3 endingLocation = new Vector3(4, 0, 8);
+
+            //Create the outside walls
+            for (int x = 0; x <= xMax - 1; x++)
+            {
+                for (int z = 0; z <= zMax - 1; z++)
+                {
+                    if (x == 0 || z == 0 || x == xMax - 1 || z == zMax - 1)
+                    {
+                        map[x, z] = "W";
+                    }
+
+                }
+            }
+            //Clear the starting and ending locations
+            map[(int)startingLocation.X, (int)startingLocation.Z] = "";
+            map[(int)endingLocation.X, (int)endingLocation.Z] = "";
+
+            Map = map;
+        }
+
+        private void Level2() {
+            int xMax = 9;
+            int zMax = 9;
+            string[,] map = MapCore.InitializeMap(xMax, zMax);
+            Vector3 startingLocation = new Vector3(4, 0, 0);
+            Vector3 endingLocation = new Vector3(4, 0, 8);
+
+            //Create the outside walls
+            for (int x = 0; x <= xMax - 1; x++)
+            {
+                for (int z = 0; z <= zMax - 1; z++)
+                {
+                    if (x == 0 || z == 0 || x == xMax - 1 || z == zMax - 1)
+                    {
+                        map[x, z] = "W";
+                    }
+
+                }
+            }
+
+            //Create an inner skinny wall with a door in the middle
+            map[1, 4] = "w";
+            map[2, 4] = "w";
+            map[3, 4] = "w";
+            map[4, 4] = "d";
+            map[5, 4] = "w";
+            map[6, 4] = "w";
+            map[7, 4] = "w";
+
+            //Clear the starting and ending locations
+            map[(int)startingLocation.X, (int)startingLocation.Z] = "";
+            map[(int)endingLocation.X, (int)endingLocation.Z] = "";
+
+            Map = map;
+        }
+
+    }
+}
